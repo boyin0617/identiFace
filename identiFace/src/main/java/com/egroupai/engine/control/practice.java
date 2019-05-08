20378ebbbef9fe8651a0dedcb0ac9a85c92d0b3d
boyin <boyin@MSI>
2019-05-08 17:38:04 +0800

    測試git

--- identiFace/src/main/java/com/egroupai/engine/control/practice.java
+++ identiFace/src/main/java/com/egroupai/engine/control/practice.java
@@ -8,15 +8,6 @@ public class practice {
   * @param args
   */
  public static void main(String[] args) {
-  // TODO Auto-generated method stub
-//	 class B {
-//		 int a;
-//		 public int fun()
-//		 {return a;}}
-//		 public class a{
-//		 public static void main(String[] args){
-//		 B b=new B();
-//		 int y=b.fun();
-//		 System.out.println(y);}}
+//6666666666666666666666666666666666666666666666666666666666666666
 }
 }
