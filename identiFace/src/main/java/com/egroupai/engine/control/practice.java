d5bc2e1d31e65c5cd79e318c64402773e27d62aa
boyin <boyin@MSI>
2019-05-21 01:58:50 +0800

    2019/5/21

--- identiFace/src/main/java/com/egroupai/engine/control/practice.java
+++ identiFace/src/main/java/com/egroupai/engine/control/practice.java
@@ -17,7 +17,7 @@ import io.opencensus.stats.Aggregation.Count;
 public class practice {
 
 	public static void main(String[] args) {
-		//123123
+		//123123123
 //		Scanner input = new Scanner(System.in);
 //		int time = input.nextInt();
 //		int shift = input.nextInt();
